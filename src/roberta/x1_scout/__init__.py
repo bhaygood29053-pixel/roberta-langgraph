85b35f25ee46bf54dea332957f019bacd9d3792b
Sungjinwoo <254147247+bhaygood29053-pixel@users.noreply.github.com>
2026-08-17 09:12:53 -0400

    Map X1 Scout reports to policy facts

--- src/roberta/x1_scout/__init__.py
+++ src/roberta/x1_scout/__init__.py
@@ -1,6 +1,15 @@
 """X1 Scout chain-specialist boundary."""
 
 from roberta.x1_scout.graph import build_x1_scout_graph
+from roberta.x1_scout.policy_facts import (
+    extract_x1_policy_facts,
+    x1_policy_facts_from_state,
+)
 from roberta.x1_scout.tool import build_x1_scout_tool
 
-__all__ = ["build_x1_scout_graph", "build_x1_scout_tool"]
+__all__ = [
+    "build_x1_scout_graph",
+    "build_x1_scout_tool",
+    "extract_x1_policy_facts",
+    "x1_policy_facts_from_state",
+]
