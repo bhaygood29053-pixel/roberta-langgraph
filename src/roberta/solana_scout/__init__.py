7466bc9711c21099af5954466f5c2b934d4a41a6
Sungjinwoo <254147247+bhaygood29053-pixel@users.noreply.github.com>
2026-08-17 10:05:49 -0400

    Export Solana policy fact adapters

--- src/roberta/solana_scout/__init__.py
+++ src/roberta/solana_scout/__init__.py
@@ -1,6 +1,10 @@
 """Solana Scout specialist boundary."""
 
 from roberta.solana_scout.graph import build_solana_scout_graph
+from roberta.solana_scout.policy_facts import (
+    extract_solana_policy_facts,
+    solana_policy_facts_from_state,
+)
 from roberta.solana_scout.state import SolanaScoutReport, SolanaScoutState
 from roberta.solana_scout.tool import build_solana_scout_tool
 
@@ -9,4 +13,6 @@ __all__ = [
     "SolanaScoutState",
     "build_solana_scout_graph",
     "build_solana_scout_tool",
+    "extract_solana_policy_facts",
+    "solana_policy_facts_from_state",
 ]
